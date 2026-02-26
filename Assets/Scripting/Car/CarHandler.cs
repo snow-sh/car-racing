e0d62768d95df33464f2e6f540ab8f4e26872a37
RileyWren <zainab.42847@gmail.com>
2026-02-26 19:59:20 +0600

    idk

--- Assets/Scripting/Car/CarHandler.cs
+++ Assets/Scripting/Car/CarHandler.cs
@@ -1,93 +1,22 @@
-// using UnityEngine;
-
-// public class CarHandler : MonoBehaviour
-// {
-//     [SerializeField] Rigidbody rb;
-//     public float speed = 2000f;
-//     public float turnSpeed = 100f;
-
-//     private float moveInput;
-//     private float steerInput;
-
-//     public void SetInput(float move, float steer)
-//     {
-//         moveInput = move;
-//         steerInput = steer;
-//     }
-
-//     // void FixedUpdate()
-//     // {
-
-//     //     // If we are giving input but the car isn't moving, give it a tiny kick
-//     // if (Mathf.Abs(moveInput) > 0.1f && rb.linearVelocity.magnitude < 0.1f)
-//     // {
-//     //     // This 'Mode.Impulse' helps break the initial friction
-//     //     rb.AddForce(transform.forward * moveInput * speed * 0.5f, ForceMode.Impulse);
-//     // }
-
-//     // // Normal driving code
-//     // Vector3 force = transform.forward * moveInput * speed;
-//     // rb.AddForce(force, ForceMode.Acceleration);
-//     //     // 1. Driving Force
-//     //     // Vector3 force = transform.forward * moveInput * speed;
-//     //     // rb.AddForce(force, ForceMode.Acceleration);
-
-//     //     // 2. Steering (Only turn if moving)
-//     //     if (rb.linearVelocity.magnitude > 0.1f)
-//     //     {
-//     //         float turn = steerInput * turnSpeed * Time.fixedDeltaTime;
-//     //         // Reverse steering if driving backward
-//     //         if (moveInput < 0) turn = -turn; 
-            
-//     //         rb.MoveRotation(rb.rotation * Quaternion.Euler(0, turn, 0));
-//     //     }
-//     // }
-
-//     void FixedUpdate()
-// {
-//     // Change ForceMode to Acceleration - this ignores the Mass value!
-//     Vector3 force = transform.forward * moveInput * speed;
-//     rb.AddForce(force, ForceMode.Acceleration);
-
-//     if (rb.linearVelocity.magnitude > 0.1f)
-//     {
-//         float turn = steerInput * turnSpeed * Time.fixedDeltaTime;
-//         if (moveInput < 0) turn = -turn; 
-//         rb.MoveRotation(rb.rotation * Quaternion.Euler(0, turn, 0));
-//     }
-// }
-
-//     void Start()
-// {
-//     if (rb != null)
-//     {
-//         rb.WakeUp(); // Forces the physics engine to pay attention to this car
-//     }
-// }
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
 using UnityEngine;
 
 public class CarHandler : MonoBehaviour
 {
+    [Header("Setup - Drag these here!")]
     [SerializeField] Rigidbody rb;
-    
-    // Adjusted values for ForceMode.Acceleration
-    public float speed = 30f;       // Much lower! 2000 was too high.
+    public Camera playerCam;        
+    public AudioSource engineSound; 
+
+    [Header("Movement Settings")]
+    public float speed = 30f;
     public float turnSpeed = 120f;
-    public float maxVelocity = 50f; // This prevents the car from flying away
+    public float maxVelocity = 50f;
+    
+    [Header("Visual & Audio Juice")]
+    public float minFOV = 60f;
+    public float maxFOV = 80f;
+    public float minPitch = 0.8f;
+    public float maxPitch = 2.5f;
 
     private float moveInput;
     private float steerInput;
@@ -100,44 +29,48 @@ public class CarHandler : MonoBehaviour
 
     void Start()
     {
-        if (rb != null) rb.WakeUp();
+        if (rb != null) 
+        {
+            rb.WakeUp();
+            rb.centerOfMass = new Vector3(0, -1f, 0); 
+        }
     }
 
-void FixedUpdate()
-{
-    // 1. Calculate Local Velocity
-    // This tells us how much we are moving Forward vs Sideways
-    Vector3 localVelocity = transform.InverseTransformDirection(rb.linearVelocity);
-
-    // 2. Kill Sideways Velocity (The "Grip" Secret)
-    // This stops the 'ice skating' effect
-    localVelocity.x *= 0.1f; // Adjust (0.1f to 0.9f) - lower is more grip
-    rb.linearVelocity = transform.TransformDirection(localVelocity);
-
-    // 3. Drive Force
-    if (Mathf.Abs(moveInput) > 0.1f)
+    void Update()
     {
-        rb.AddForce(transform.forward * moveInput * speed, ForceMode.Acceleration);
+        float speedFactor = rb.linearVelocity.magnitude / maxVelocity;
+
+        if (playerCam != null)
+        {
+            playerCam.fieldOfView = Mathf.Lerp(minFOV, maxFOV, speedFactor);
+        }
+
+        if (engineSound != null)
+        {
+            engineSound.pitch = Mathf.Lerp(minPitch, maxPitch, speedFactor);
+        }
     }
 
-    // 4. Improved Steering (Only turn if moving)
-    if (rb.linearVelocity.magnitude > 1f)
+    void FixedUpdate()
     {
-        float turn = steerInput * turnSpeed * Time.fixedDeltaTime;
-        // Check if we are reversing to flip steering
-        if (localVelocity.z < 0) turn = -turn;
 
-        rb.MoveRotation(rb.rotation * Quaternion.Euler(0, turn, 0));
-    }
-}
+        Vector3 localVelocity = transform.InverseTransformDirection(rb.linearVelocity);
+        localVelocity.x *= 0.1f; 
+        rb.linearVelocity = transform.TransformDirection(localVelocity);
 
-void OnCollisionEnter(Collision collision)
-{
-    if (collision.gameObject.CompareTag("Grass"))
-    {
-        Debug.Log("Get off the grass!");
-        // Optional: Reset scene or reduce health
-        UnityEngine.SceneManagement.SceneManager.LoadScene(0); 
+    
+        if (Mathf.Abs(moveInput) > 0.1f)
+        {
+            rb.AddForce(transform.forward * moveInput * speed, ForceMode.Acceleration);
+        }
+
+        if (rb.linearVelocity.magnitude > 1f)
+        {
+            float turn = steerInput * turnSpeed * Time.fixedDeltaTime;
+            if (localVelocity.z < 0) turn = -turn; 
+            rb.MoveRotation(rb.rotation * Quaternion.Euler(0, turn, 0));
+        }
+
+        rb.AddForce(-transform.up * 10f * rb.linearVelocity.magnitude);
     }
-}
 }
